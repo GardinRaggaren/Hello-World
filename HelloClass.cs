f06ab8a9bf73d50133762c1a1fb1d2328e81a894
GardinRaggaren <kevin.aspegren@elev.ga.lbs.se>
2017-01-20 09:48:24 +0100

    helloworld

---
+++ HelloClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_
+{
+    class HelloClass
+    {
+        public void Hello()
+        {
+            string Hw = "Hello world!";
+
+            Console.WriteLine(Hw);
+        }
+
+    }
+}
